cb24b53821c6407a2e288a5f05505d3f6e896bf9
Colleen <colleenmacklin@gmail.com>
2020-12-29 19:47:37 -0500

    Better Clouds
    In Game Opening, you can see the results of the new double-layer cloud with texture instead of sprite shape - this fixes problems with rendering detailed silhouettes with holes or cutouts.
    also, added a test script to dynamically change the  underlying shape.

---
+++ Assets/Scripts/test_script.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_script : MonoBehaviour
+{
+    public GameObject hiCloud;
+    public GameObject lowCloud;
+    public string hiName; //store the name of the underlying shape here
+    public Texture2D newShape;
+    public Texture2D currentShapeT;
+    public bool changeShape;
+    private ParticleSystem hps; 
+    private ParticleSystem lps;
+    public ParticleSystem.ShapeModule myShapeH;
+    public ParticleSystem.ShapeModule myShapeL;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hps = hiCloud.GetComponent<ParticleSystem>();
+        lps =lowCloud.GetComponent<ParticleSystem>();
+        myShapeH = hps.shape;
+        myShapeL = lps.shape;
+        currentShapeT = hps.shape.texture;
+
+        hiName = hps.shape.texture.name;
+
+        if (changeShape)
+        {
+            Debug.Log("changing shape");
+            myShapeH.texture = newShape;
+            myShapeL.texture = newShape;
+
+            //hps.shape.texture = newShape;
+
+        }
+    
+
+    //shape.texture = Assets.GetBuiltinExtraResource<Texture2D>("Default-Particle.psd");
+
+}
+
+// Update is called once per frame
+void Update()
+{
+}
+}
+

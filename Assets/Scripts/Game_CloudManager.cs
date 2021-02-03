d2a703aff55180c52602f4048950bd8efc5fee3f
Jason Li <jasonli1337@gmail.com>
2021-02-03 02:55:59 -0500

    CloudManager Updates
    Cloud Manager now uses
    Some clearer fields, List for clouds.
    Removed SetRange in favor of fields
    Active Clouds can now be set
    Clarity to Cloud Spawner function name
    
    ToDo:
    -Remove gamelogic from gamecloudlayergroup.cs
    -Add all textures to gameclouds
    -Cloud Manager needs to take control over cloud logic

--- Assets/Scripts/Game_CloudManager.cs
+++ Assets/Scripts/Game_CloudManager.cs
@@ -5,15 +5,18 @@ using UnityEngine.Events;
 
 public class Game_CloudManager : MonoBehaviour
 {
-	public GameObject cloudGroup;
-	public Texture2D[] ShapeArray;
-    public bool isShape;
-    public bool timerReset;
+    [Header("Cloud Properties")]
+    [SerializeField]
+    private GameObject cloudGroup;
+    [SerializeField]
+    private Texture2D[] ShapeArray; //textures stay as an array because we are not generating run time textures
     public GameObject chosenCloud;
-    public int cloudNum;
     public Texture2D chosenShape;
-    public GameObject[] CloudArray;
-    private GameObject[] chosenCloudArray; //keep track of the clouds that have been chosen
+
+    [SerializeField]
+    private int numberOfClouds;
+    [SerializeField]
+    private List<GameObject> ActiveClouds;
     public GameObject shapeCollider; //our testing box that we resuse, will be a GameObject with only a collider on it
     public Bounds shapeBounds;
     public int instantiationAttempts = 50; //how many instantiation attempts we should make before failing
@@ -21,21 +24,21 @@ public class Game_CloudManager : MonoBehaviour
     public int shapeCount;// how many shapes to show before ending game?
                           //timer
 
+    [Header("Spawning Attributes")]
     // Ranges for positioning clouds when they spawn (left/right, far/near, up/down)
-    private Vector3 Min;
-    private Vector3 Max;
-    private float _xAxis;
-    private float _yAxis;
-    private float _zAxis; //If you need this, use it
-    private Vector3 _randomPosition;
-    public bool _canInstantiate;
+    [SerializeField]
+    private Vector2 xRange, yRange, zRange;
 
-    private float sMin;
-    private float sMax;
-    private float scaleNum;
-    private Vector3 scaleChange;
+    [SerializeField]
+    private Vector2 scaleRange;
+    private void SetRanges()
+    {
+        // Min = new Vector3(-30, 25, -40); //Random location value not behind trees.
+        // Max = new Vector3(40, 35, 70); //Another random value, not behind trees. - Y used to be 40
+        // sMin = 1;
+        // sMax = 2.0f; //changed from 2
+    }
 
-  
     //behaviors:
     //tell EventManager to "SpawnShape" (CloudArray[n], ShapeArray[n])
     //tell EventManager to remove "SpawnShape" (CloudArray[n])
@@ -53,38 +56,39 @@ public class Game_CloudManager : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-	{
-        //start some kind of timer?
-        //set the possible locations for clouds
-        SetRanges();
-        for (int i = 0; i < CloudArray.Length; i++)
-        {
-            CloudArray[i] = InstantiateRandomObjects(); // should return a gameObject
-            CloudArray[i].name = "cloud" + i;
-        }
-        shapeCloud();
-
-
-    }
-    //possible locations for clouds /scales
-    private void SetRanges()
     {
-        Min = new Vector3(-30, 25, -40); //Random location value not behind trees.
-        Max = new Vector3(40, 35, 70); //Another random value, not behind trees. - Y used to be 40
-        sMin = 1;
-        sMax = 2.0f; //changed from 2
+        CreateActiveClouds();
+        SetCloudToShape();
     }
 
-    private void shapeCloud()
+    void CreateActiveClouds()
+    {
+        ActiveClouds = new List<GameObject>();
+        for (int i = 0; i < numberOfClouds; i++)
+        {
+            var go = SpawnRandomNonIntersectingCloud(ActiveClouds);
+            go.name = $"cloud {i}";
+            ActiveClouds.Add(go);
+        }
+    }
+
+    private void SetCloudToShape()
     {
         //choose a random cloud to turn into a shape
         int shapeNum = (Random.Range(0, ShapeArray.Length));
+        int cloudNum = (Random.Range(0, ActiveClouds.Count));
         chosenShape = ShapeArray[shapeNum];
-        cloudNum = (Random.Range(0, CloudArray.Length));
-        chosenCloud = CloudArray[cloudNum];
+        chosenCloud = ActiveClouds[cloudNum];
         Debug.Log("chosenCloud: " + chosenCloud);
 
-        TurnOffCloud(); //tells cloud that it is not a shape anymore, changes it's shape...
+        //tells cloud that it is not a shape anymore, changes it's shape...
+        TurnOffCloud();
+
+        //This needs to be adjusted from the SpawnShape
+        //Right now the chosenCloud is compared against from the clouds?
+        //We should change this paradigm so that the shape is directly set
+        //Because the manager, if it exists at all, should manage. Not respond.
+        //i.e. clouds should not be responsible for knowing they are chosen
 
         EventManager.TriggerEvent("SpawnShape"); //tell a cloud to turn into a shape
 
@@ -105,77 +109,63 @@ public class Game_CloudManager : MonoBehaviour
         //call this on a timer, or set of co-routines
         if (Input.GetKeyDown("o"))
         {
-            shapeCloud();
+            SetCloudToShape();
         }
 
 
     }
     //for prefab instantiation, see: https://docs.unity3d.com/Manual/InstantiatingPrefabs.html
 
-    private GameObject InstantiateRandomObjects() //need to check to make sure these clouds are not too close / intersecting
+    private GameObject SpawnRandomNonIntersectingCloud(List<GameObject> currentClouds) //need to check to make sure these clouds are not too close / intersecting
     {
-            //check for collisions first
+        //check for collisions first
 
-
-            //set random locations
-            //Debug.Log("_xAxis: " + _yAxis);
-            for (int n = 0; n < instantiationAttempts; n++)
-            {
+        Vector3 _randomPosition = new Vector3();
+        Vector3 scaleChange = new Vector3();
+        //set random locations
+        for (int n = 0; n < instantiationAttempts; n++)
+        {
 
             Debug.Log("Instantiation Attempts: " + n);
 
             //set up random location
-            _xAxis = Random.Range(Min.x, Max.x);
-                _yAxis = Random.Range(Min.y, Max.y);
-                _zAxis = Random.Range(Min.z, Max.z);
-                _randomPosition = new Vector3(_xAxis, _yAxis, _zAxis);
+            float _xAxis = Random.Range(xRange.x, xRange.y);
+            float _yAxis = Random.Range(yRange.x, yRange.y);
+            float _zAxis = Random.Range(zRange.x, zRange.y);
+            _randomPosition = new Vector3(_xAxis, _yAxis, _zAxis);
 
-                scaleNum = Random.Range(sMin, sMax);
-                scaleChange = new Vector3(scaleNum, scaleNum, scaleNum);
+            float scaleNum = Random.Range(scaleRange.x, scaleRange.y);
+            scaleChange = new Vector3(scaleNum, scaleNum, scaleNum);
 
-                //first check for possible collisions
-                //Step 1: random position
-                shapeCollider.transform.localScale = scaleChange;
-                shapeCollider.transform.position = _randomPosition; //move collider to position
-                shapeBounds = shapeCollider.GetComponent<Collider>().bounds;//get the collider
+            //first check for possible collisions
+            //Step 1: random position
+            shapeCollider.transform.localScale = scaleChange;
+            shapeCollider.transform.position = _randomPosition; //move collider to position
+            shapeBounds = shapeCollider.GetComponent<Collider>().bounds;//get the collider
 
-                //Step 2: compare position against active positions
-                var valid = true; //how we will know if we have a valid shape
-                                  //compare our testing box to every other active cloud shape's
-                foreach (var cloud in CloudArray)
+            //Step 2: compare position against active positions
+            var valid = true; //how we will know if we have a valid shape
+                              //compare our testing box to every other active cloud shape's
+            foreach (var cloud in currentClouds)
+            {
+                //if intersection found, then not valid
+                if (shapeBounds.Intersects(cloud.GetComponent<Collider>().bounds))
                 {
-                    //if intersection found, then not valid
-                    if (shapeBounds.Intersects(cloud.GetComponent<Collider>().bounds))
-                    {
                     Debug.Log("INTERSECTION");
-                        valid = false;
-
+                    valid = false;
                     break;
-                    }
                 }
-                if (valid) break; //if it is valid, then we leave 
+            }
+            if (valid) break; //if it is valid, then we leave 
 
         }
 
         //step 3: create cloud
-        var go = (GameObject)Instantiate(cloudGroup, _randomPosition, Quaternion.EulerRotation(-90, 0, 0));
-            //var go = Instantiate(prefab); //create prefab at position
-            go.transform.localScale = scaleChange;
+        GameObject go = (GameObject)Instantiate(cloudGroup, _randomPosition, Quaternion.Euler(-90, 0, 0), transform);
+        go.transform.localScale = scaleChange;
 
-            return go;
-        /*
+        return go;
 
-                    for (int i = 0; i < CloudArray.Length; i++)
-                        {
-                            //set up scales
-
-
-                            CloudArray[i] = (GameObject)Instantiate(cloudGroup, _randomPosition, Quaternion.EulerRotation(-90, 0, 0)); ; //clouds are always rotated to show their face to the person lying down (x = -90)
-                            CloudArray[i].name = "Cloud" + i;
-                            CloudArray[i].transform.localScale = scaleChange;
-                            //Debug.Log("cloud instantiated: " + CloudArray[i].name);
-                        }
-        */
 
     }
 }

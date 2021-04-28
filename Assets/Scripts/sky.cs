cae866f1369a08594c026dd88122ed1bc72a190f
Colleen <colleenmacklin@gmail.com>
2021-04-28 12:44:20 -0400

    Sunrise in opening scene

--- Assets/Scripts/sky.cs
+++ Assets/Scripts/sky.cs
@@ -1,32 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace UnityEngine.AzureSky {
-
-public class sky : MonoBehaviour
+namespace UnityEngine.AzureSky
 {
-    //public AzureSkyController azureSky;
-    // Start is called before the first frame update
-    void OnEnable()
-    {
-        //EventManager.StartListening("sunrise", sunrise);
-    }
 
-    void OnDisable()
+    public class sky : MonoBehaviour
     {
-        //EventManager.StopListening("sunrise", sunrise);
-    }
 
-    void Start()
-    {
-            //azureSky.SetTimelineSourceTransitionTime(10);
-            //azureSky.StartTimelineTransition(17);
-    }
+        public AzureTimeController azureTimeController;
+        public int start_time = 5;
+        public int end_time = 15;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+        // Start is called before the first frame update
+        void OnEnable()
+        {
+            EventManager.StartListening("sunrise", sunrise);
+        }
+
+        void OnDisable()
+        {
+            EventManager.StopListening("sunrise", sunrise);
+        }
+
+        void Start()
+        {
+            sunrise();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void sunrise()
+        {
+            azureTimeController.SetTimeline(start_time);
+            azureTimeController.StartTimelineTransition(end_time, 0, 3, AzureTimeDirection.Forward);
+        }
+
     }
 }
-}
+

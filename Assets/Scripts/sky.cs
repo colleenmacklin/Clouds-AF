f1d436abce94959c1c2c8bc29ec5db07b8b341e6
kahnivore <kahnivore@gmail.com>
2023-02-10 16:03:17 -0800

    sky script changes

--- Assets/Scripts/sky.cs
+++ Assets/Scripts/sky.cs
@@ -10,9 +10,11 @@ namespace UnityEngine.AzureSky
         public AzureTimeController azureTimeController;
         public AzureWeatherController weatherController;
         public int sunrise_start_time = 5;
-        public int sunrise_end_time = 12;
+        public int sunrise_end_time_hour = 12;
+        public int sunrise_end_time_minute = 0;
         public int sunset_start_time;
-        public int sunset_end_time = 24;
+        public int sunset_end_time_hour = 24;
+        public int sunset_end_time_minute = 0;
         public float timeScale = 0.005f;
         //TODO: tweak the AzureSky materials stuff to make better colaration for sunrise and sunset
 
@@ -50,14 +52,13 @@ namespace UnityEngine.AzureSky
             }
             */
         }
-
+        
         private void sunrise() //consider converting to an action with the timeScale set by gameManager/cloudManager
         {
             //TODO: Slow the sunrise speed to conform to loading the GPT model
             Debug.Log("Starting Sunrise");
-            timeScale = 0.005f;
             azureTimeController.SetTimeline(sunrise_start_time);
-            azureTimeController.StartTimelineTransition(sunrise_end_time, 0, timeScale, AzureTimeDirection.Forward);
+            azureTimeController.StartTimelineTransition(sunrise_end_time_hour, sunrise_end_time_minute, timeScale, AzureTimeDirection.Forward);
 
             /*
             Vector2 time_of_day = azureTimeController.GetTimeOfDay();
@@ -77,7 +78,7 @@ namespace UnityEngine.AzureSky
             var current_hour = azureTimeController.GetTimeOfDay().x;
             var current_minute = azureTimeController.GetTimeOfDay().y;
             azureTimeController.SetTimeline(current_hour);
-            azureTimeController.StartTimelineTransition(sunset_end_time, 0, 1.5f, AzureTimeDirection.Forward);
+            azureTimeController.StartTimelineTransition(sunset_end_time_hour, 0, 1.5f, AzureTimeDirection.Forward);
 
 
         }

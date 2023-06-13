bca9933f3780b70814e22e498418ec70004ef371
kahnivore <kahnivore@gmail.com>
2023-06-12 23:52:26 -0400

    start of buttefly rot

--- Assets/Scripts/FollowCursor.cs
+++ Assets/Scripts/FollowCursor.cs
@@ -6,12 +6,19 @@ public class FollowCursor : MonoBehaviour
 {
     private Vector3 _mousePos;
 
-   
+    private Vector3 _butterflyRotInitial;
+
     private Camera _camera;
+
+
+    private Vector3 _normalizedMousePos;
 
     private void Awake()
     {
+
+        _butterflyRotInitial = this.transform.eulerAngles;
         
+
         foreach(Camera c in Camera.allCameras)
         {
             if (c.gameObject.name == "ButterflyCam")
@@ -22,16 +29,24 @@ public class FollowCursor : MonoBehaviour
        
     }
 
+    Vector3 delta = Vector3.zero;
+    Vector3 lastPos = Vector3.zero;
     private void Update()
     {
         _mousePos = Input.mousePosition;
-      
-
         Vector3 newPos = _camera.ScreenToWorldPoint(_mousePos);
+       
         transform.position = _camera.ScreenToWorldPoint(new Vector3(_mousePos.x, _mousePos.y, 100)) ;
 
-    }
+        
 
+        delta = (_mousePos - lastPos);
+       // Debug.Log("delta is " + delta);
+        //transform.Rotate( delta);
+
+        lastPos = Input.mousePosition;
+    }
+        
 
 
 

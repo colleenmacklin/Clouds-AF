48ab35a50046ba020ba955aa7e40c1819fa23b2f
iteratiii <44853931+iteratiii@users.noreply.github.com>
2023-07-17 13:25:38 -0400

    tweak butterfly circle formula

--- Assets/ButterflyCircle.cs
+++ Assets/ButterflyCircle.cs
@@ -12,9 +12,9 @@ public class ButterflyCircle : MonoBehaviour
 
         //make butterfly move in a circle over time
         timeCounter += Time.deltaTime;
-        float x = Mathf.Cos(timeCounter);
-        float y = Mathf.Sin(timeCounter);
+        float x = Mathf.Cos(timeCounter*2);
+        float y = Mathf.Sin(timeCounter*2);
         float z = 100;
-        transform.position = new Vector3(x, y, z);
+        transform.position = new Vector3((x * 4 / 5)+1, y * 4 / 5, z);
     }
 }
